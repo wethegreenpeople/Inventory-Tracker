73fb99d69c63de6a851997cd2c46d15a00f1c744
wethegreenpeople <superjohn007@gmail.com>
2015-06-12 12:09:06 -0700

    Added a server update/downloading the latest inventory from the internet
    Switched over to Linux and got curl to start working.
    
    Tried a bunch of ways to do cross-platform screen clears and ended up going with the evil system calls. Might change that in the future depending on how ambitious I am.
    
    Code looks ugly right now, I messed around with it a lot changing a bunch of stuff around, will clean it up in the future.
    
    There are a few redundancies in the code, so I'm going to clear that up aswell.

--- main.cc
+++ main.cc
@@ -6,16 +6,25 @@ This is pretty barebones thus far as I haven't implemented any actual downloadin
 #include <string>
 #include <stdlib.h>
 #include <fstream>
-#include <windows.h>
+#include <unistd.h>
+#include <cstdlib>
 #include <curl/curl.h>
+
+void clearScreen(){
+	#ifdef _WIN32
+		std::system("cls");
+	#else
+		std::system("clear");
+	#endif
+}
 
 void menu(){
 	// Main Menu
-	system("cls");
+	clearScreen();	
 	std::cout << "Your inventory information: \n\n" << std::endl;
 	std::cout << "(1) Check latest inventory" << std::endl;
 	std::cout << "(2) Update local inventory" << std::endl;
-	std::cout << "(3) Update server inventory" << std::endl;
+	std::cout << "(3) Download server inventory" << std::endl;
 	std::cout << "(4) Check Archives" << std::endl;
 	std::cout << "(5) Exit \n" << std::endl;
 	std::cout << "Selection: ";
@@ -28,20 +37,21 @@ void miniMenu(){
 	if (miniMenuSelection == 1){
 		menu();
 	}
+	else {
+		clearScreen();
+	}
 }
 
 void checkInvt(){
-	SetCurrentDirectory("stock");
 	std::ifstream in("current.txt");
 	std::string contents((std::istreambuf_iterator<char>(in)), 
 	    std::istreambuf_iterator<char>());
-	system("cls");
+	clearScreen();
 	std::cout << contents << std::endl;
 }
 
 void updateInvt(){
-	system("cls");
-	SetCurrentDirectory("stock");
+	clearScreen();
 	std::string file = "notepad.exe current.txt";
 	system(file.c_str());
 }
@@ -52,20 +62,54 @@ int main(){
 	menu();
 	std::cin >> menuSelection;
 
+	// Defining where a couple of often used paths are
+	const char *const path = "stock/";
+	const char *const file = "current.txt";
+
 	// Check inventory. Displays what you currently have.
 	if (menuSelection == 1){
+		chdir ((path));
 		checkInvt();
 		miniMenu();
 	}
 
 	// Update what you currently have, local only, nothing server wise
 	if (menuSelection == 2){
+		chdir((path));
 		updateInvt();
 		checkInvt();
 		miniMenu();
 	}
 
-	// Update 
-	
+	// Download server file 
+	if (menuSelection == 3){
+		clearScreen();
+
+		CURL *curl;
+		CURLcode res;
+
+		curl = curl_easy_init();
+		if(curl){
+			curl_easy_setopt(curl, CURLOPT_URL, "http://sansassimports.com/current.txt");
+
+			res = curl_easy_perform(curl);
+
+			// Check for errors
+			if (res != CURLE_OK){
+				std::cerr << "Sorry, didn't work" << std::endl;
+			}
+
+			curl_easy_cleanup(curl);
+			miniMenu();
+		}
+
+
+	} 
+
+	// Exit program
+	else{
+		clearScreen();
+	}
+
 	return 0;
 }
